1b7ed0631e1f28f6589090f24d8ed3f9c43009cb
David Triana <jdavidgtriana@gmail.com>
2025-07-11 20:15:42 -0700

    working on the base of the main menu

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("hola mundo");
+let header = document.getElementById("main-header");
+const scrollThreshold = 50; // px scrolled before shrinking
+window.addEventListener('scroll', () => {
+    if (!header)
+        return; // Ensure header exists
+    if (window.scrollY > scrollThreshold) {
+        header.classList.add('scrolled');
+    }
+    else {
+        header.classList.remove('scrolled');
+    }
+});
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("hola mundo");
+let header = document.getElementById("main-header") as HTMLElement;
+const scrollThreshold = 50;  // px scrolled before shrinking
+
+window.addEventListener('scroll', () => {
+    if (!header) return; // Ensure header exists
+    if (window.scrollY > scrollThreshold) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
